bccaec27445a14b837d1456ce461d90acaa0beda
Olivia Toolson <livytoolson@gmail.com>
2020-11-09 16:59:57 -0700

    created login form

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 import './App.css';
+
+import Login from './components/Login';
+import Friends from './components/FriendForm';
 
 function App() {
   return (
-    <div className="App">
-      <h1>friends app</h1>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          {/* <li>
+            <Link to="/protected">Friends</Link>
+          </li> */}
+        </ul>
+        <Switch>
+          {/* <Route exact path="/protected" component={Friends}/> */}
+          <Route path="/login" component={Login}/>
+          <Route component={Login}/>
+        </Switch>
+      </div>
+    </Router>
+    );
 }
 
 export default App;
